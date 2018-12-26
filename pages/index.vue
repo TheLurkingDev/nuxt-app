8606b0ff5f9c92e6b916bffcce8656d7dde109f8
Darren Cook <thelurkingdev@gmail.com>
2018-12-25 22:29:36 -0500

    Using 'nuxt-link' for spa routing.

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,7 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        simple_nuxt_app
-      </h1>
-      <h2 class="subtitle">
-        My bedazzling Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+    <div>      
+      <nuxt-link to="/blog">Blogs</nuxt-link>
     </div>
   </section>
 </template>
